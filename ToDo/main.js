dc299656a4c2eaa7bdeb2d0bc6839df3aa7de768
magic-db <dominik.birchler@gmx.ch>
2025-01-05 16:33:58 +0100

    main.js

--- ToDo/main.js
+++ ToDo/main.js
@@ -1,17 +1,16 @@
-
 let todo = JSON.parse(localStorage.getItem("todo")) || [];
 const todoInput = document.getElementById("todoInput");
 const todoList = document.getElementById("todoList");
 const todoCount = document.getElementById("todoCount");
-const addButton = document.querySelector(".btn");
+const addButton = document.getElementById("addButton");
 const deleteButton = document.getElementById("deleteButton");
-
+const taskPriority = document.getElementById("task-priority");
 
 document.addEventListener("DOMContentLoaded", function () {
   addButton.addEventListener("click", addTask);
   todoInput.addEventListener("keydown", function (event) {
     if (event.key === "Enter") {
-      event.preventDefault(); 
+      event.preventDefault();
       addTask();
     }
   });
@@ -21,8 +20,9 @@ document.addEventListener("DOMContentLoaded", function () {
 
 function addTask() {
   const newTask = todoInput.value.trim();
+  const priority = taskPriority.value;
   if (newTask !== "") {
-    todo.push({ text: newTask, disabled: false });
+    todo.push({ text: newTask, disabled: false, priority: priority });
     saveToLocalStorage();
     todoInput.value = "";
     displayTasks();
@@ -33,22 +33,30 @@ function displayTasks() {
   todoList.innerHTML = "";
   todo.forEach((item, index) => {
     const p = document.createElement("p");
+    const priorityClass = getPriorityClass(item.priority);
     p.innerHTML = `
-      <div class="todo-container">
-        <input type="checkbox" class="todo-checkbox" id="input-${index}" ${
-      item.disabled ? "checked" : ""
-    }>
-        <p id="todo-${index}" class="${
-      item.disabled ? "disabled" : ""
-    }" onclick="editTask(${index})">${item.text}</p>
+      <div class="todo-container ${priorityClass}">
+        <input type="checkbox" class="todo-checkbox" id="input-${index}" ${item.disabled ? "checked" : ""}>
+        <p id="todo-${index}" class="${item.disabled ? "disabled" : ""}" onclick="editTask(${index})">${item.text}</p>
       </div>
     `;
-    p.querySelector(".todo-checkbox").addEventListener("change", () =>
-      toggleTask(index)
-    );
+    p.querySelector(".todo-checkbox").addEventListener("change", () => toggleTask(index));
     todoList.appendChild(p);
   });
   todoCount.textContent = todo.length;
+}
+
+function getPriorityClass(priority) {
+  switch (priority) {
+    case "high":
+      return "high-priority";
+    case "medium":
+      return "medium-priority";
+    case "low":
+      return "low-priority";
+    default:
+      return "";
+  }
 }
 
 function editTask(index) {
